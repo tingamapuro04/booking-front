22736a45bbbd75ca1b0c7891bf1654b1157bb34a
tingamapuro04 <alponcemobutu100@gmail.com>
2023-03-04 15:50:08 +0300

    try posting a coach

--- src/components/CoachForm.jsx
+++ src/components/CoachForm.jsx
@@ -1,27 +1,99 @@
 /* eslint-disable jsx-a11y/label-has-associated-control */
-import React from 'react';
+import React, { useState } from 'react';
 
 const CoachForm = () => {
   const mobutu = 'Mobutu';
-  // const [name, setName] = useState('');
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [city, setCity] = useState('');
+  const [image, setImage] = useState('');
+
+  const resetForm = () => {
+    setName('');
+    setDescription('');
+    setCity('');
+    setImage(null);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('http://localhost:3001/coaches',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          coach: {
+            name,
+            description,
+            city,
+            image,
+          },
+        }),
+      })
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error('Network response was not ok.');
+      })
+      .then((response) => console.log(response))
+      .catch(() => console.log('There was an error.'));
+    resetForm();
+  };
+
   return (
     <div className="container w-75">
       <p>{mobutu}</p>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div className="mb-3">
-          <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
-          <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
-          <div id="emailHelp" className="form-text">We&apos;ll never share your email with anyone else.</div>
+          <label htmlFor="exampleInputEmail1" className="form-label">
+            Coach Name
+          </label>
+          <input
+            onChange={(e) => setName(e.target.value)}
+            type="text"
+            className="form-control"
+            id="exampleInputEmail1"
+          />
         </div>
         <div className="mb-3">
-          <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
-          <input type="password" className="form-control" id="exampleInputPassword1" />
+          <label htmlFor="description" className="form-label">
+            Description
+          </label>
+          <input
+            onChange={(e) => setDescription(e.target.value)}
+            type="text"
+            className="form-control"
+            id="description"
+          />
         </div>
-        <div className="mb-3 form-check">
-          <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-          <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
+        <div className="mb-3">
+          <label htmlFor="city" className="form-label">
+            Coach City
+          </label>
+          <input
+            onChange={(e) => setCity(e.target.value)}
+            type="text"
+            className="form-control"
+            id="city"
+          />
         </div>
-        <button type="submit" className="btn btn-primary">Submit</button>
+        <div className="mb-3">
+          <label htmlFor="city" className="form-label">
+            Coach Image
+          </label>
+          <input
+            onChange={(e) => setImage(e.target.files[0])}
+            type="file"
+            className="form-control"
+            id="inputGroupFile02"
+          />
+        </div>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
       </form>
     </div>
   );
