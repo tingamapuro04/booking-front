9bf46811313bd3902061859b9574181f29601265
tingamapuro04 <alponcemobutu100@gmail.com>
2023-03-09 23:12:26 +0300

    homepage

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
           <Route path="/home" element={<Home />} />
           <Route path="/signup" element={<SignUp />} />
           <Route
-            path="coaches"
+            path="/coaches"
             element={<Coaches />}
           />
           <Route
