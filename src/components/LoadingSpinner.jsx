ba1cd616dc601f666636c99bc8247d114c745f66
tingamapuro04 <alponcemobutu100@gmail.com>
2023-04-29 16:03:34 +0300

    Second commit

--- src/components/LoadingSpinner.jsx
+++ src/components/LoadingSpinner.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
+import './Loading.css';
 
-const LoadingSpinner = () => {
-  return (
-    <div className='spinner-container'>
-      <div className='spinner' >
+const LoadingSpinner = () => (
+  <div className="spinner-container">
+    <div className="spinner" />
+  </div>
+);
 
-      </div>
-    </div>
-  )
-}
-
-export default LoadingSpinner
+export default LoadingSpinner;
